8bbae2b5694c82035e2a4dd96436344e009ecf4d
AlekseevKA <yfafcayfaa@gmail.com>
2022-03-03 09:49:36 +0900

    qwe

--- app/src/main/java/com/example/doskajava/MainActivity.java
+++ app/src/main/java/com/example/doskajava/MainActivity.java
@@ -1,16 +1,36 @@
 package com.example.doskajava;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.core.Tag;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private NavigationView nav_view;
+    private DrawerLayout drawerLayout;
+    private FirebaseAuth mAuth;
+
 
 
     @Override
@@ -23,6 +43,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     {
         nav_view = findViewById(R.id.nav_view);
         nav_view.setNavigationItemSelectedListener(this);
+        drawerLayout = findViewById(GravityCompat.START);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Tabla");
+
+        myRef.setValue("Hello, World!");
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 
     @Override
@@ -44,15 +79,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
 
             case R.id.id_my_smartphone_ads:
-                Toast.makeText(this, "Pressed id smapt", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Pressed id smart", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_dm_ads:
                 Toast.makeText(this, "Pressed id dm", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_up:
+                signUpDialog(R.string.sign_up, R.string.sign_up_button, 0);
                 Toast.makeText(this, "Pressed id sign up", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_in:
+                signUpDialog(R.string.sign_in, R.string.sign_in_button, 1);
                 Toast.makeText(this, "Pressed id sign in", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.id_sign_out:
@@ -61,5 +98,64 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         }
         return true;
+    }
+
+    private void signUp(String email, String password)
+    {
+        if(!email.equals("") && !password.equals("")) {
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if(user != null)Log.d("MyLogMainActivity", "createUserWithEmail:success " + user.getEmail());
+
+
+                            } else {
+                                Log.w("MyLogMainActivity", "createIserWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "Authentification failed",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
+        }else
+        {
+            Toast.makeText(this, "Email or Password is empty!", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+    private void signUpDialog(int title, int buttonTitle, int index)
+    {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.sign_up_layout, null);
+        dialogBuilder.setView(dialogView);
+        TextView titleTextView = dialogView.findViewById(R.id.tvAlertTitle);
+
+        EditText edEmail = dialogView.findViewById(R.id.edEmail);
+        EditText edPassword = dialogView.findViewById(R.id.edPassword);
+
+
+        titleTextView.setText(title);
+        Button b = dialogView.findViewById(R.id.buttonSignUp);
+        b.setText(buttonTitle);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            if (index == 0)
+            {
+                signUp(edEmail.getText().toString(), edPassword.getText().toString());
+            }
+            }
+        });
+        AlertDialog dialog = dialogBuilder.create();
+        dialog.show();
+
     }
 }
